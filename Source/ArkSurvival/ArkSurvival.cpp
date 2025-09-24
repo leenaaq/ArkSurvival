03fc5af06b7479e75e84ead2e6953a5c157cf428
leenaaq <v584731@gmail.com>
2025-09-24 10:14:42 +0900

    [Add] Validation macros

--- Source/ArkSurvival/ArkSurvival.cpp
+++ Source/ArkSurvival/ArkSurvival.cpp
@@ -4,3 +4,4 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArkSurvival, "ArkSurvival" );
+DEFINE_LOG_CATEGORY(LogCharacter);
--- Source/ArkSurvival/ArkSurvival.h
+++ Source/ArkSurvival/ArkSurvival.h
@@ -4,3 +4,10 @@
 
 #include "CoreMinimal.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogCharacter, Log, All);
+
+#define ARK_VALIDATE(Expression) \
+if (!(Expression)) { \
+UE_LOG(LogCharacter, Warning, TEXT("Validation Failed: %s in %s"), TEXT(#Expression), ANSI_TO_TCHAR(__FUNCTION__)); \
+return; \
+}
